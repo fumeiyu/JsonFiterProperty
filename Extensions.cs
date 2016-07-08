363b6e841d8a486d8d51e270ea9f96d20eda589e
fumeiyu <fumeiyu@gmail.com>
2016-07-08 17:42:43 +0800

    最初版本

---
+++ Extensions.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Json
+{
+    public static class Extensions
+    {
+
+        /// <summary>
+        /// 转换成JSON
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="souce">对象</param>
+        /// <param name="fiterProperty">过滤掉的属性</param>
+        /// <returns></returns>
+        public static string ToJson<T>(this T souce,string[] fiterProperty) where T : class, new()
+        {
+            var settings = new JsonSerializerSettings();
+
+            //加上ContractResolver，正向表列哪些属性要序列化
+            settings.ContractResolver =
+            new LimitPropsContractResolver(fiterProperty);
+
+            //Formatting.Indented 可读性好
+            return JsonConvert.SerializeObject(souce, settings);
+        }
+    }
+}
